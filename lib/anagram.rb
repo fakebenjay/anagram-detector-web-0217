8f0be48bc1270d1ba756d6342d3c139380fa90aa
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-08 21:57:55 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    match_array = []
+    array.each do |array_word| #figure out how to do this properly with #collect
+      if array_word.split("").sort == @word.split("").sort
+        match_array << array_word
+      end
+    end
+    match_array
+  end
+
+end
